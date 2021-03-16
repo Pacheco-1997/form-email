c8a1f2a9fcfa9b7a9c0a58bb58e484355051e1ba
Victor Pacheco <33486039+Pacheco-1997@users.noreply.github.com>
2021-03-16 17:02:07 -0300

    Adicionando no github
    Adicionando no github

---
+++ frontend/manda_img.js
@@ -0,0 +1,38 @@
+
+  
+  const doUpload = (url, options) => {
+      const promiseCallback = (resolve, reject) => {
+          fetch(url, options)
+          .then(response => response.json())
+          .then(resolve)
+          .catch(reject)
+      }
+      return new Promise(promiseCallback);
+  }
+  
+  
+  const uploadImage = (e) => {
+       e.preventDefault();
+
+       const file = document.getElementById('file-img');
+
+       const data = new FormData();
+       data.append('image', file.files[0]);
+
+
+       doUpload('https://localhost:44371/api/ImageUpload', {
+           method: 'POST',
+           body: data,
+           headers: {
+            'Content-Type': 'application/json'
+           }
+         
+       })
+       .then(console.log)
+       .catch(console.error)
+   }
+
+
+   const form = document.getElementById('upload-form');
+   console.log(form)
+   form.addEventListener('submit', uploadImage)
